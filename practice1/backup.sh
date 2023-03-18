361c9658269ee4779104e1a8470cce4dad2fd15a
Oleksii Linnyk <oleksii.linnyk@suntechinnovation.com>
2023-03-18 18:55:01 +0200

    practice1

---
+++ practice1/backup.sh
@@ -0,0 +1,5 @@
+echo "[-------[`date +%F--%H-%M`]-----------]"
+
+tar -cvf /Users/oleksiilinnyk/khpi/devops/practice1/backup/backup.tar /Users/oleksiilinnyk/khpi/devops/practice1/userFiles.txt
+
+exit 0
